250a8c1f4811495a2b15ae180a82839e8a1d8e15
sarcastic <rursik@gmail.com>
2015-03-12 20:38:15 +0300

    Initial Commit

---
+++ Collages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Collages
+//
+//  Created by Ekaterina Belinskaya on 12/03/15.
+//  Copyright (c) 2015 Ekaterina Belinskaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Collages/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Collages
+//
+//  Created by Ekaterina Belinskaya on 12/03/15.
+//  Copyright (c) 2015 Ekaterina Belinskaya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
